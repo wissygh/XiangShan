da83eca2e4a61dc9c99bbd2018c53c837fffa6e6
Xuan Hu <huxuan@bosc.ac.cn>
2023-06-20 22:44:53 +0800

    tmp

--- src/main/scala/xiangshan/backend/issue/MultiWakeupQueue.scala
+++ src/main/scala/xiangshan/backend/issue/MultiWakeupQueue.scala
@@ -44,5 +44,5 @@ class MultiWakeupQueue[T <: Data, TFlush <: Data](
   io.deq.valid := pipesValidVec.asUInt.orR
   io.deq.bits := Mux1H(pipesValidVec, pipesBitsVec)
 
-  assert(PopCount(pipesValidVec) > 1.U, "PopCount(pipesValidVec) should be no more than 1")
+//  assert(PopCount(pipesValidVec) > 1.U, "PopCount(pipesValidVec) should be no more than 1")
 }
